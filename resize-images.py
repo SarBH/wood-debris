e5405197ecee9cf73030e0eeb5d244103b4bb5bd
SarBH <sbalayla@stevens.edu>
2019-12-15 12:34:12 -0500

    initial commit

---
+++ resize-images.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+
+orig_path = 'data/unlabeled/standard objects only' # path to the original full sized images
+resized_path = 'data/unlabeled/standard-objects-resized' # path to place resized images
+
+# iterate through all images in path
+for filename in os.listdir(orig_path):
+    img = cv2.imread(os.path.join(orig_path, filename))
+    print('Orig. dimensions', img.shape)
+
+    # resize images by scale_percent %
+    scale_percent = 25
+    width = int(img.shape[1] * scale_percent/100)
+    height = int(img.shape[0] * scale_percent/100)
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    print('Resized dimensions', resized.shape)
+
+    # enter name for resized image: I keep the same name with a _r at the end
+    new_img_name = filename[:-4] + '_r.JPG'
+
+    # export the resized image
+    cv2.imwrite(os.path.join(resized_path, new_img_name), resized)
