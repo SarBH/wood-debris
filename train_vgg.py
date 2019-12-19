b0ced2b7cc63dc6cc3dd0bd74d9088d5b994730e
SarBH <saritahedaya@gmail.com>
2019-12-19 14:15:50 -0500

    explained on comment RTX script

--- train_vgg.py
+++ train_vgg.py
@@ -1,14 +1,18 @@
 # from image_segmentation_keras_local import keras_segmentation_local
 
-
 from image_segmentation_keras_local.keras_segmentation_local.models.unet import vgg_unet
 from image_segmentation_keras_local.keras_segmentation_local import __init__
 
 __init__
 
+
+# MUST ALWAYS PASTE THIS ON EVERY SCRIPT BECAUSE RTX CARDS REQUIRE IT
+#   otherwise will see this error: 
+        #   UnknownError:  Failed to get convolution algorithm. 
+        #   This is probably because cuDNN failed to initialize, 
+        #   so try looking to see if a warning log message was printed above.
 from keras.backend.tensorflow_backend import set_session
 import tensorflow as tf
-
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 print(physical_devices)
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
