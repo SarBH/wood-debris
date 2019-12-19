438ff2591178ead597aaace313e102ff8bdc0a5b
SarBH <saritahedaya@gmail.com>
2019-12-19 12:12:28 -0500

    changed labels of classes to [0,4]

--- helper-code/convert_to_mask.py
+++ helper-code/convert_to_mask.py
@@ -17,13 +17,13 @@ def convert_to_mask(x_coord, y_coord, sample_name, class_name, ann_img):
     
     # depending of the class of that region, paint it a different shade
     if class_name == "branch":
-        cv2.fillConvexPoly(ann_img, all_poly_coords, 2)
+        cv2.fillConvexPoly(ann_img, all_poly_coords, 1)
     if class_name == "box":
-        cv2.fillConvexPoly(ann_img, all_poly_coords, 3)
+        cv2.fillConvexPoly(ann_img, all_poly_coords, 2)
     if class_name == "camera-bag":
-        cv2.fillConvexPoly(ann_img, all_poly_coords, 4)
+        cv2.fillConvexPoly(ann_img, all_poly_coords, 3)
     if class_name == "tree":
-        cv2.fillConvexPoly(ann_img, all_poly_coords, 5)
+        cv2.fillConvexPoly(ann_img, all_poly_coords, 4)
 
     # return mask with added region
     return ann_img
@@ -35,17 +35,17 @@ def convert_to_mask(x_coord, y_coord, sample_name, class_name, ann_img):
 if __name__ == "__main__":
     
     # define the dimensions of the original images, masks must keep the same dimensions
-    IMG_SHAPE = (540,960)
+    IMG_SHAPE = (544,960)
 
     # open json file that came from via annotations. Deselect all three options when downloading
-    with open('data/train/via_region_data.json') as mask_json:
+    with open('data/train/via-region-data-july-24.json') as mask_json:
         json_file = json.load(mask_json)
         
         # iterate through all "image" objects to extract data
         for image in json_file["_via_img_metadata"].values():
             filename = image["filename"]
             
-            prev_mask = np.ones((IMG_SHAPE[0], IMG_SHAPE[1]))
+            prev_mask = np.zeros((IMG_SHAPE[0], IMG_SHAPE[1]))
             
             # fail-safe: if no regions were defined for an image, skip it
             if image["regions"] == []:
@@ -54,7 +54,7 @@ if __name__ == "__main__":
             # iterate through all regions to create masks
             for region in image["regions"]:
 
-                class_name = region["region_attributes"]["name"]
+                class_name = region["region_attributes"]["class_name"]
                 x_coord = region["shape_attributes"]["all_points_x"]
                 y_coord = region["shape_attributes"]["all_points_y"]
                 
@@ -64,4 +64,4 @@ if __name__ == "__main__":
             
             # when done with all regions defined for an image, save the mask.
             image_tensor = np.dstack([masked_image, masked_image, masked_image])
-            cv2.imwrite(str("data/train/masks/"+filename+".png"), image_tensor)
+            cv2.imwrite(str("data/train/masks/"+filename[:-4]+".png"), image_tensor)
